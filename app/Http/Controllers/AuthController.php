fdc081a838f8921e054b15d9932d339c88ca0c76
Tidiane Badji <btidianed004@gmail.com>
2024-05-05 19:21:44 +0000

    feat: update message otp

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Twilio\Rest\Client;
+
 
 
 class AuthController extends Controller
@@ -37,6 +39,16 @@ class AuthController extends Controller
         return $this->respondWithToken($token);
     }
 
+    // public function register(RegisterRequest $request)
+    // {
+    //     $user = new User();
+    //     $user->first_name = $request->first_name;
+    //     $user->last_name = $request->last_name;
+    //     $user->telephone = $request->telephone;
+    //     $user->password = Hash::make($request->password);
+    //     $user->save();
+    //     return response()->json(['Inscription effectuée avec success']);
+    // }
     public function register(RegisterRequest $request)
     {
         $user = new User();
@@ -45,8 +57,16 @@ class AuthController extends Controller
         $user->telephone = $request->telephone;
         $user->password = Hash::make($request->password);
         $user->save();
-        return response()->json(['Inscription effectuée avec success']);
+
+        // Générer un OTP aléatoire
+        $otp = rand(100000, 999999); // OTP à 6 chiffres
+
+        // Envoyer l'OTP via SMS
+        $this->sendOTP($user->telephone, "Votre code OTP est: $otp");
+
+        return response()->json(['Inscription effectuée avec success', 'otp' => $otp]); // Vous pouvez retirer 'otp' du JSON pour la production
     }
+
     /**
      * Get the authenticated User.
      *
@@ -94,4 +114,25 @@ class AuthController extends Controller
             'expires_in' => auth()->factory()->getTTL() * 60,
         ]);
     }
+
+function sendOTP($phone, $message) {
+    $sid = env('TWILIO_SID');
+    $token = env('TWILIO_TOKEN');
+    $client = new Client($sid, $token);
+
+    try {
+        $client->messages->create(
+            $phone,
+            [
+                'from' => env('TWILIO_FROM'),
+                'body' => $message
+            ]
+        );
+    } catch (\Exception $e) {
+        return false; // Gérer l'exception ou logger l'erreur
+    }
+
+    return true;
+}
+
 }
